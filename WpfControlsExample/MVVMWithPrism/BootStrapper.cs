c65edfe001fb27f465d641247ca9aa017a3ffe56
Sujai Sureshbabu <sujaisuresh23@gmail.com>
2020-02-18 23:40:21 -0400

    Wpf Controls - Commit 2

---
+++ WpfControlsExample/MVVMWithPrism/BootStrapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Modularity;
+using Prism.Unity;
+
+namespace MVVMWithPrism
+{
+    public class BootStrapper : UnityBootstrapper
+    {
+        public override void Run(bool runWithDefaultConfiguration)
+        {
+            base.Run(runWithDefaultConfiguration);
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            App.Current.MainWindow = (Window) Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog(); //Use this once module is created
+
+            Type sampleModuleType = typeof(SamplePrismModule.SampleModule);
+            ModuleCatalog.AddModule(new ModuleInfo()
+            { ModuleName = sampleModuleType.Name, ModuleType = sampleModuleType.AssemblyQualifiedName });
+        }
+    }
+}
---
+++ WpfControlsExample/SamplePrismModule/SampleModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace SamplePrismModule
+{
+    public class SampleModule : IModule
+    {
+        private IRegionManager _regionManager;
+
+        public SampleModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            //containerProvider.Resolve(typeof(Views.SampleView), "MainRegion");
+            _regionManager.RegisterViewWithRegion("MainRegion", typeof(Views.SampleView));
+            //containerProvider.
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //containerRegistry.Register(typeof(SampleModule), "MainRegion");
+            //throw new NotImplementedException();
+        }
+    }
+}
